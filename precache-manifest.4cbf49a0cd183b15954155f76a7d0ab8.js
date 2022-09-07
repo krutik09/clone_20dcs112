7259ec8e39af3456f64a7e4dd0e68f0ba329c2b3
krutik <81080689+krutik09@users.noreply.github.com>
2022-09-07 13:22:56 +0530

    Updates

---
+++ precache-manifest.4cbf49a0cd183b15954155f76a7d0ab8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92d549ef8b2d9dfc06e1ef103752682d",
+    "url": "/clone-20dcs112/index.html"
+  },
+  {
+    "revision": "5204efd19cd4bb25612c",
+    "url": "/clone-20dcs112/static/css/2.a1840750.chunk.css"
+  },
+  {
+    "revision": "f305f111f33482cbad09",
+    "url": "/clone-20dcs112/static/css/main.6777df22.chunk.css"
+  },
+  {
+    "revision": "5204efd19cd4bb25612c",
+    "url": "/clone-20dcs112/static/js/2.29c5b276.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/clone-20dcs112/static/js/2.29c5b276.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f305f111f33482cbad09",
+    "url": "/clone-20dcs112/static/js/main.9b2776d7.chunk.js"
+  },
+  {
+    "revision": "55eeff3cab3758eac015",
+    "url": "/clone-20dcs112/static/js/runtime-main.441adf23.js"
+  }
+]);
